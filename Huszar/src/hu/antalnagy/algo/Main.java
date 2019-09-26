1cf3f6bde31438d1e3daa2ef0013b47f16ec9113
p1nkpengw1n <sm4le99tony@gmail.com>
2019-09-26 13:50:08 +0200

    Created Huszar

---
+++ Huszar/src/hu/antalnagy/algo/Main.java
@@ -0,0 +1,9 @@
+package hu.antalnagy.algo;
+
+public class Main {
+    public static void main(String[] args) {
+        Board board = new Board(5);
+        board.placeKnight("A2");
+        board.show();
+    }
+}
