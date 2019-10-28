4e5721d1e40083b48c3fdfa67dd1946ce8a24a96
p1nkpengw1n <sm4le99tony@gmail.com>
2019-10-28 12:56:27 +0100

    Gitignore issue fixed

---
+++ Connect-Four/src/hu/antalnagy/connect4/Controller.java
@@ -0,0 +1,41 @@
+package hu.antalnagy.connect4;
+
+import javax.swing.*;
+import java.util.Objects;
+
+class Controller {
+
+    Controller(View v, Model m) {
+
+        for(JButton[] buttonRow : v.gridButtons) {
+            for(JButton button : buttonRow) {
+                button.setEnabled(false);
+            }
+        }
+        for(JButton button : v.borderButtons) {
+            button.addActionListener(e -> {
+                boolean isWin = m.handleButton(Integer.parseInt(button.getText()));
+                if(isWin) {
+                    m.popUpAlertWin();
+                    Application.newGame("SMALL");
+                    v.setVisible(false); //was told it is a good practice, not sure of its necessity
+                    v.dispose();
+                }
+                else if(m.isDraw()) {
+                    m.popUpAlertDraw();
+                    Application.newGame("SMALL");
+                    v.setVisible(false);
+                    v.dispose();
+                }
+            });
+        }
+        v.comboBox.addActionListener(e -> {
+            JComboBox cb = (JComboBox) e.getSource();
+            String[] size = ((String) Objects.requireNonNull(cb.getSelectedItem())).split("_");
+            Application.newGame(size[0]);
+            v.setVisible(false);
+            v.dispose();
+        });
+
+    }
+}
