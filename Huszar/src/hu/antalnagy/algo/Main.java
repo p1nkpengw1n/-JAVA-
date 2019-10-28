1d989548ed0d6e631aaf1a0587b01010e0ca629e
p1nkpengw1n <sm4le99tony@gmail.com>
2019-10-28 12:40:12 +0100

    Add files via upload

--- Huszar/src/hu/antalnagy/algo/Main.java
+++
@@ -1,9 +0,0 @@
-package hu.antalnagy.algo;
-
-public class Main {
-    public static void main(String[] args) {
-        Board board = new Board(5);
-        board.placeKnight("A2");
-        board.show();
-    }
-}
